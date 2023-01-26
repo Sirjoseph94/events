54595c2111ea23e6039b7a1c99e8975446f87696
Joseph Ero <erojoseph94@gmail.com>
2023-01-26 23:51:11 +0100

    chore(API response): refactored the events endpoint response [Fixes #184320644]

--- src/utils/handleResponse.ts
+++ src/utils/handleResponse.ts
@@ -1,12 +1,20 @@
 import { Response } from "express";
-export function success(res: Response, statusCode:number, data: Record<string, any>) {
-  return res.status(statusCode).json({
+export function success(
+  res: Response,
+  statusCode: number,
+  data?: Record<string, any> | string
+) {
+  return res.status(statusCode || 304).json({
     status: "success",
     message: data,
   });
 }
 
-export function failed(res: any, statusCode:number, data: Record<string, any>) {
+export function failed(
+  res: any,
+  statusCode: number,
+  data: Record<string, any>
+) {
   return res.status(statusCode).json({
     status: "error",
     message: data,
