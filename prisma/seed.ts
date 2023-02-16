3b5d877bff3bc319fb809db4dd58695b64f63839
Joseph Ero <erojoseph94@gmail.com>
2023-02-16 02:26:32 +0100

    chore(database) seed the database with admin and user account

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,21 +2,29 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const seed = async () => {
-  const admin = await prisma.user.create({
-    data: {
+  const admin = await prisma.user.upsert({
+    create: {
       name: "Admin Jo",
       email: "admin@mail.com",
       password: "admin123",
       isAdmin: true,
     },
+    update: {},
+    where: {
+      email: "admin@mail.com",
+    },
   });
 
-  const user = await prisma.user.create({
-    data: {
+  const user = await prisma.user.upsert({
+    create: {
       name: "User Jo",
       email: "user@mail.com",
       password: "user123",
       isAdmin: false,
+    },
+    update: {},
+    where: {
+      email: "user@mail.com",
     },
   });
   console.log({ admin, user });
